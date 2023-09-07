8058873b432a6149f9dc32e05718f5ef5087a4eb
munim <muhammadmunimoff330@gmail.com>
2023-09-07 11:04:33 +0500

    var & fun names updated

--- app/page.js
+++ app/page.js
@@ -2,41 +2,39 @@
 import React, { useState } from 'react'
 
 const page = () => {
-  const [title, settitle] = useState("")
-  const [desc, setdesc] = useState("")
-  const [maintask, setmaintask] = useState([])
+  const [title, setTitle] = useState("")
+  const [desc, setDesc] = useState("")
+  const [mainTask, setMainTask] = useState([])
 
-  const submithandler = (e) => {
+  const submitHandler = (e) => {
     e.preventDefault()
-    // console.log(title);
-    // console.log(desc);
-    setmaintask([...maintask, {
+    setMainTask([...mainTask, {
       title, desc
     }])
-    settitle("")
-    setdesc("")
-    // console.log(maintask);
+    setTitle("")
+    setDesc("")
   }
 
-  const deletehandler = (index) => {
-    let copyTask = [...maintask]
+  const deleteHandler = (index) => {
+    let copyTask = [...mainTask]
     copyTask.splice(index, 1)
-    setmaintask(copyTask)
+    setMainTask(copyTask)
   }
 
   let renderTask = <h2 className='text-white'>No Task Available</h2>
 
-  if (maintask.length > 0) {
-    renderTask = maintask.map((task, index) => {
+  if (mainTask.length > 0) {
+    renderTask = mainTask.map((task, index) => {
       return (
         <li key={index} className='flex justify-between '>
-          {/* <div className='flex items-center justify-between mb-2'> */}
-            <h5 className='text-white text-2xl font-semibold'>{task.title}</h5>
-            <h6 className='text-white text-2xl font-semibold'>{task.desc}</h6>
-          {/* </div> */}
-          <button className='text-white bg-red-400 p-2 rounded mb-2'  onClick={() => 
-            deletehandler(index)
+
+          <h5 className='text-white text-2xl font-semibold'>{task.title}</h5>
+          <h6 className='text-white text-2xl font-semibold'>{task.desc}</h6>
+
+          <button className='text-white bg-red-400 p-2 rounded mb-2' onClick={() =>
+            deleteHandler(index)
           }> Delete </button>
+
         </li>
       )
     })
@@ -45,11 +43,11 @@ const page = () => {
   return (
     <>
       <h1 className='bg-black text-white p-5 text-4xl font-bold text-center'>My Todo List</h1>
-      <form onSubmit={submithandler}>
+      <form onSubmit={submitHandler}>
         <input type="text" className='text-2xl border-zinc-800 border-2 m-3 px-2 py-1' placeholder='Enter Title Here' required
-          value={title} onChange={(e) => { settitle(e.target.value) }} />
+          value={title} onChange={(e) => { setTitle(e.target.value) }} />
         <input type="text" className='text-2xl border-zinc-800 border-2 m-3 px-2 py-1' placeholder='Enter Description Here' required
-          value={desc} onChange={(e) => { setdesc(e.target.value) }} />
+          value={desc} onChange={(e) => { setDesc(e.target.value) }} />
         <button className='bg-black text-white px-2 py-1 text-xl rounded m-3'>Add Task</button>
       </form>
 
